0305f8bfa264983804afb2e2bb2d28fa27ffa311
Alex456 <alejandrofernandezheredia34@gmail.com>
2025-06-10 18:15:27 -0300

    ale
    echo en gdevelop

---
+++ code3.js
@@ -0,0 +1,20 @@
+gdjs.inicioCode = {};
+gdjs.inicioCode.localVariables = [];
+
+
+gdjs.inicioCode.eventsList0 = function(runtimeScene) {
+
+};
+
+gdjs.inicioCode.func = function(runtimeScene) {
+runtimeScene.getOnceTriggers().startNewFrame();
+
+
+gdjs.inicioCode.eventsList0(runtimeScene);
+
+
+return;
+
+}
+
+gdjs['inicioCode'] = gdjs.inicioCode;
